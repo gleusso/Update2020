e4d57c477482cde3da666bec173601f768628143
Gleusso sa silva sa teles <gleussosateles@yahoo.com>
2020-08-11 15:14:51 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -10,5 +10,27 @@ namespace SalesWebMvc.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> sellers { get; set; } = new List<Seller>();
+
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeles(Seller seller)
+        {
+            sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
