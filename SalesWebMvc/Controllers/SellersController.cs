f968d868141f7a3d0e7cad433a90abcb4dda0816
Gleusso sa silva sa teles <gleussosateles@yahoo.com>
2020-08-17 14:36:47 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -44,6 +44,16 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create(Seller seller)
         {
+                 
+                if (!ModelState.IsValid)
+                {
+
+                    var departments = _departmentService.FindAll();
+                    var viewModel = new SellerFormViewModel { seller = seller, Departments = departments };
+                    return View(viewModel);
+                }
+
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -112,6 +122,17 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+
+            if (!ModelState.IsValid)
+            {
+
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { seller = seller, Departments = departments };
+
+
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
